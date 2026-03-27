e0593c93ea588695a9f1160abd720faec10797ab
minhyeong0904 <58597846+minhyeong0904@users.noreply.github.com>
2026-03-27 17:02:07 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.ET_CONFIG = {
+  SUPABASE_URL: "https://ynjohdnpxeborztjhvpy.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inluam9oZG5weGVib3J6dGpodnB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NTEzNjUsImV4cCI6MjA5MDEyNzM2NX0.2Qn4ZvP4tIetCWnZDRTqjY1CVKSmJ-VBm1X8Tb-cYnA",
+  TABLE_NAME: "tracker_data"
+};
